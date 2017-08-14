de4f6dd28d1596735344cf2ac68acc44ec135880
Michele Kempinsky <mkempinsky@icloud.com>
2017-08-14 12:05:17 -0700

    start of working with the loop

--- functions.php
+++ functions.php
@@ -3,7 +3,9 @@
 // function to load style sheets
 function wpt_theme_styles(){
 	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css');
-	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css');
+	// wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css');
+	// only need normalize_css if you plan on making further customizations on the theme
+	// and want to normalize some of the styles and font sizes.
 	wp_enqueue_style( 'google_fonts', get_template_directory_uri() . 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic');
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php get_header(); ?>
 
 
-
+	<?php 
+	// opening of the loop:
+	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+	?>
+	    <section class="row">
+	      <div class="small-12 columns text-center">
+	        <div class="leader">
+	          <h1>Etiam Mollis Sit Gratt Spore.</h1>
+	          <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue.</p>        
+	        </div>
+	      </div>
+	    </section>
+	<?php
+	// closing of the loop
+	endwhile; else; ?>
+		<p><?php _e('sorry, no posts matched your criteria.'); ?></p>
+	<?php endif; ?>
 <?php get_footer(); ?>

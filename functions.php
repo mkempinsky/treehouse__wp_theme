1c785afcf274f51d99a2a87c29e22f067b4a1f49
Michele Kempinsky <mkempinsky@icloud.com>
2017-08-14 10:14:14 -0700

    notes in functions.php

--- functions.php
+++ functions.php
@@ -8,15 +8,19 @@ function wpt_theme_styles(){
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
 
-// call the function to load the themes
+// call the function to load styles
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles');
 
+// function to load js
 function wpt_theme_js(){
+	// calling false places the script in the header, asks 'is in footer?' empty '' for version number
 	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/vendor/modernizr.js', '', '', false);
 	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation/foundation.js', array('jquery'), '', true);
+	// following takes array of dependencies -- calling true puts script in footer
 	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true);
 }
 
+// call the function to load js
 add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
 
 ?>

685648543325202c122b7cc726be6b05b9bdb576
Michele Kempinsky <mkempinsky@icloud.com>
2017-08-14 13:11:04 -0700

    loop

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php get_header(); ?>
 
 
-	<?php 
-	// opening of the loop:
-	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-	?>
-	    <section class="row">
-	      <div class="small-12 columns text-center">
-	        <div class="leader">
-	          <h1>Etiam Mollis Sit Gratt Spore.</h1>
-	          <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue.</p>        
-	        </div>
-	      </div>
-	    </section>
-	<?php
-	// closing of the loop
-	endwhile; else; ?>
-		<p><?php _e('sorry, no posts matched your criteria.'); ?></p>
-	<?php endif; ?>
+<section class="row">
+	<div class="small-12 columns text-center">
+		<div class="leader">
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		  <h1><?php the_title(); ?></h1>
+		  <p><?php the_content(); ?></p>
+
+		<?php endwhile; else; ?>
+
+			<p><?php _e('sorry, no posts matched your criteria.'); ?></p>
+
+		<?php endif; ?>
+
+		</div>
+	</div>
+</section>
+
 <?php get_footer(); ?>
